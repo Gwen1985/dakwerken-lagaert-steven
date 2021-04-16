7198f5846e4c578202840524213971b04a6fc6cd
gwen1985 <gwen.ellegeest@outlook.com>
2021-04-17 01:04:04 +0200

    some finetune in the home page

--- src/views/HomeContent.js
+++ src/views/HomeContent.js
@@ -11,55 +11,57 @@ const HomeContent = () => {
 
     return (
         <div className="homeContent">
+
             <div className="row">
-                <div className="container">
 
-                    <div className="col-sm-12 pt-3 bg-white text-center">
-                        <div className="container">
-                            <h2 className="contentTitle text-secondary pt-5">DAKWERKEN LAGAERT STEVEN UIT
-                                EVERGEM</h2>
-                            <h2 className="contentTitle_2 pt-3">Uw dak tot in de puntjes afgewerkt</h2>
-                            <div className="contenttextBox text-center">
-                                <p className="contentText p-2">Is uw hellende of plat dak aan vervanging toe? Of
-                                    wilt u een
-                                    volledig
-                                    nieuw dak laten
-                                    plaatsen? <br/>
-                                    <br/>
-                                    Dakwerken Lagaert Steven klaart de klus voor u!</p>
+                <div className="col-sm-12 col-lg-6 pt-3 bg-white text-center">
 
-                                <p className="contentText p-2">Zelfwerkend zaakvoerder Steven Lagaert heeft meer
-                                    dan
-                                    twaalf jaar
-                                    ervaring en slaat voor zijn
-                                    projecten de handen in elkaar met drie andere zelfstandigen. Steven
-                                    garandeert u
-                                    steeds
-                                    een
-                                    goede
-                                    prijs-kwaliteit en duurzame eindresultaten.</p>
-                            </div>
+                    <div className="container pt-3">
+                        <h2 className="contentTitle text-secondary pt-5 ">DAKWERKEN LAGAERT STEVEN UIT
+                            EVERGEM</h2>
+                        <h2 className="contentTitle_2 pt-3">Uw dak tot in de puntjes afgewerkt</h2>
 
-                            <Link to="/contact" className="offerte">offerte aanvragen <i
-                                className="fas fa-arrow-right"/></Link>
+                        <div className="container contenttextBox text-center p-5 mt-3">
+                            <p className="contentText">Is uw hellend of plat dak aan vervanging toe? Of
+                                wilt u een
+                                volledig
+                                nieuw dak laten
+                                plaatsen? <br/>
+                                <br/>
+                                Dakwerken Lagaert Steven klaart de klus voor u!</p>
+
+                            <p className="contentText">Zelfwerkend zaakvoerder Steven Lagaert heeft meer
+                                dan
+                                twaalf jaar
+                                ervaring en slaat voor zijn
+                                projecten de handen in elkaar met drie andere zelfstandigen. Steven
+                                garandeert u
+                                steeds
+                                een
+                                goede
+                                prijs-kwaliteit en duurzame eindresultaten.</p>
+
+                            <Link to="/contact" className="offerte">
+                                offerte aanvragen
+                                <i className="fas fa-arrow-right ml-2"/>
+                            </Link>
                         </div>
-                    </div>
 
-
-                    <div className="col-sm-12 pt-3 bg-white text-center ">
-                        <div className="container">
-
-                            <div className="imageWrapper p-5">
-                                <img src={werkenOpDak} alt="werken_op_dak" width="100%" className="werkenOpDak"
-                                     loading="lazy"/>
-                                <img src={dakRamen} alt="dak_ramen" width="100%" className="dakRamen"
-                                     loading="lazy"/>
-                            </div>
-
-                        </div>
                     </div>
 
                 </div>
+
+
+                <div className="col-sm-12 col-lg-6 pt-3 bg-white text-center ">
+
+                    <div className="imageWrapper p-5">
+                        <img src={werkenOpDak} alt="werken_op_dak" width="100%" className="werkenOpDak"
+                             loading="lazy"/>
+                        <img src={dakRamen} alt="dak_ramen" width="100%" className="dakRamen"
+                             loading="lazy"/>
+                    </div>
+                </div>
+
             </div>
 
             <div className="row dakpannen">
@@ -76,109 +78,102 @@ const HomeContent = () => {
 
             </div>
 
-            <div className="row logoMidPage mt-5">
-
-                <div className="col-sm-12 p-4">
-                    <div className="container text-center p-4">
-                        <Link to="/">
-                            <img src={logo} alt="logo" width="100%" className="logoImg"/>
-                        </Link>
-                    </div>
-                </div>
+            <div className="row pt-5 d-flex justify-content-center">
 
 
-            </div>
+                <div className="col-sm-12 col-lg-6 order-lg-2 text-center">
+                    <div className="container">
+                        <h2 className="contentTitle text-secondary pt-5">DAKWERKEN LAGAERT STEVEN UIT EVERGEM</h2>
+                        <h2 className="contentTitle_2 pt-3">Talent te over</h2>
 
-            <div className="row pt-4 ">
+                        <div className="container contenttextBox text-center p-5 mt-3">
+                            <p className="contentText">Het dienstenpakket van Dakwerken Lagaert Steven bestaat
+                                uit
+                                dakwerken
+                                aan
+                                hellende en platte
+                                daken, zinkwerken, gevelbekleding en Gyprocwerken.</p>
 
-                <div className="col-sm-12 col-md-6 p-4 d-flex justify-content-center">
-                    <div className="card text-center">
-                        <i className="far fa-comments p-4"/>
-                        <h2 className="font-weight-bold">Duidelijke afspraken</h2>
-                        <p>Steven is uw centrale aanspreekpunt tijdens alle werken.</p>
-                    </div>
-                </div>
+                            <p className="contentText">Steven is uw centrale aanspreekpunt zodat u zeker kunt
+                                zijn
+                                van
+                                duidelijke afspraken. Steven maakt graag de tijd om al uw vragen en opmerkingen te
+                                beantwoorden.
+                                Dakwerken Lagaert Steven is actief in heel Oost-Vlaanderen.</p>
 
-                <div className="col-sm-12 col-md-6 p-4 d-flex justify-content-center">
-                    <div className="card text-center">
-                        <i className="fas fa-tools p-4"/>
-                        <h2 className="font-weight-bold">Veelzijdig vakwerk</h2>
-                        <p>Van dak- tot Gyprocwerken, Steven doet het allemaal.</p>
-                    </div>
-                </div>
+                            <p className="contentText">Wil u graag een afspraak maken? Dat kan op vrijdag en
+                                zaterdag.
+                                Steven bespreekt uw wensen en uw budget en kan ook een werfbezoek inplannen indien
+                                mogelijk.</p>
 
-            </div>
-
-            <div className="row pt-4">
-
-                <div className="col-sm-12 col-md-6 p-4 d-flex justify-content-center">
-                    <div className="card text-center">
-                        <i className="fab fa-envira p-4"/>
-                        <h2 className="font-weight-bold">Kwaliteitsmaterialen</h2>
-                        <p>U bent steeds zeker van duurzame resultaten</p>
-                    </div>
-                </div>
-
-                <div className="col-sm-12 col-md-6 p-4 d-flex justify-content-center">
-                    <div className="card text-center">
-                        <i className="fas fa-calendar-day p-4"/>
-                        <h2 className="font-weight-bold">Stipte planning</h2>
-                        <p>Steven volgt altijd de vooropgestelde planning.</p>
-                    </div>
-                </div>
-
-            </div>
-
-            <div className="row p-3">
-                <div className="container">
-
-                    <div className="col-12 text-center">
-                        <h2 className="talentTitle text-secondary p-3">DAKWERKEN LAGAERT STEVEN UIT EVERGEM</h2>
-                        <h2 className="talentTitle_2 p-3">Talent te over</h2>
-
-                        <p className="talentText p-2">Het dienstenpakket van Dakwerken Lagaert Steven bestaat
-                            uit
-                            dakwerken
-                            aan
-                            hellende en platte
-                            daken, zinkwerken, gevelbekleding en Gyprocwerken.</p>
-
-                        <p className="talentText p-2">Steven is uw centrale aanspreekpunt zodat u zeker kunt
-                            zijn
-                            van
-                            duidelijke afspraken. Steven maakt graag de tijd om al uw vragen en opmerkingen te
-                            beantwoorden.
-                            Dakwerken Lagaert Steven is actief in heel Oost-Vlaanderen.</p>
-
-                        <p className="talentText p-2">Wil u graag een afspraak maken? Dat kan op vrijdag en
-                            zaterdag.
-                            Steven bespreekt uw wensen en uw budget en kan ook een werfbezoek inplannen indien
-                            mogelijk.</p>
-
-                        <p className="talentText p-2">Nadien stelt Steven een gedetailleerde offerte op.
-                            Contacteer
-                            hem
-                            telefonisch, via mail of vul het contactformulier in.</p>
+                            <p className="contentText">Nadien stelt Steven een gedetailleerde offerte op.
+                                Contacteer
+                                hem
+                                telefonisch, via mail of vul het contactformulier in.</p>
+                        </div>
 
                         <Link to="/contact">
-                            <button className="btn btn-outline-dark font-weight-bold mt-2 p-3">Offerte aanvragen
+                            <button className="btn btn-outline-dark font-weight-bold p-3">Offerte aanvragen
                             </button>
                         </Link>
                     </div>
+
                 </div>
-            </div>
 
+                <div className="col-sm-12 mt-sm-3 col-lg-6 order-lg-1">
+                    <div className="container">
 
-            <div className="row dinestenWrapper">
-                <div className="container">
-                    <div className="col-12 text-center">
-                        <h2 className="dienstTitle text-white p-3">DAKWERKEN LAGAERT STEVEN UIT EVERGEM</h2>
-                        <h2 className="dienstTitle_2 text-white p-3 font-weight-bold">Onze diensten</h2>
+                        <div className="row">
+                            <div className="col-sm-12 col-md-12 col-lg-6 text-center d-flex justify-content-center">
+
+                                <div className="card text-center mt-2">
+                                    <i className="fab fa-envira p-4"/>
+                                    <h2 className="font-weight-bold">Duidelijke afspraken</h2>
+                                    <p>Steven is uw centrale aanspreekpunt tijdens alle werken.</p>
+                                </div>
+
+                            </div>
+
+                            <div className="col-sm-12 col-md-12 col-lg-6 text-center d-flex justify-content-center">
+
+                                <div className="card text-center mt-2">
+                                    <i className="fas fa-tools p-4"/>
+                                    <h2 className="font-weight-bold">Veelzijdig vakwerk</h2>
+                                    <p>Van dak- tot Gyprocwerken, Steven doet het allemaal.</p>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div className="row mt-2">
+                            <div className="col-sm-12 col-md-12 col-lg-6 text-center d-flex justify-content-center">
+
+                                <div className="card text-center mt-2">
+                                    <i className="fab fa-envira p-4"/>
+                                    <h2 className="font-weight-bold">Duidelijke afspraken</h2>
+                                    <p>Steven is uw centrale aanspreekpunt tijdens alle werken.</p>
+                                </div>
+
+                            </div>
+
+                            <div className="col-sm-12 col-md-12 col-lg-6 text-center d-flex justify-content-center">
+
+                                <div className="card text-center mt-2">
+                                    <i className="fas fa-tools p-4"/>
+                                    <h2 className="font-weight-bold">Kwaliteitsmaterialen</h2>
+                                    <p>U bent steeds zeker van duurzame resultaten</p>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
+
                 </div>
+
             </div>
 
-            <div className="row dienstenWrapper">
+
+            <div className="row dienstenWrapper mt-5">
 
                 <div className="col-sm-12 col-md-6 p-4 d-flex justify-content-center">
                     <Link to="/hellende-daken" className="text-decoration-none">
