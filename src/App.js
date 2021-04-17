8533562c8b8efbb54d14399fa54cb07abe1ef7a4
gwen1985 <gwen.ellegeest@outlook.com>
2021-04-17 13:40:13 +0200

    Add ScrollToTop for starting <Links> at top of every page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 import {Route, Switch} from "react-router";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -12,41 +12,44 @@ import GevelBekleding from "./views/GevelBekleding";
 import GyprocWerken from "./views/GyprocWerken";
 import Realisaties from "./views/Realisaties";
 import Contact from "./views/Contact";
+import ScrollToTop from "./controllers/ScrollToTop";
 
 
-const  App = () => {
+const App = () => {
 
     return (
 
         <div className="App">
 
-            <BrowserRouter>
-                <Switch>
+            <Router>
+                <ScrollToTop>
+                    <Switch>
 
-                    <Route path="/" exact component={Home}/>
+                        <Route path="/" exact component={Home}/>
 
-                    <Route path="/hellende-daken" exact component={HellendeDaken}/>
+                        <Route path="/hellende-daken" exact component={HellendeDaken}/>
 
-                    <Route path="/platte-daken" exact component={PlatteDaken}/>
+                        <Route path="/platte-daken" exact component={PlatteDaken}/>
 
-                    <Route path="/zink-werken" exact component={ZinkWerken}/>
+                        <Route path="/zink-werken" exact component={ZinkWerken}/>
 
-                    <Route path="/gevel-bekleding" exact component={GevelBekleding}/>
+                        <Route path="/gevel-bekleding" exact component={GevelBekleding}/>
 
-                    <Route path="/gyproc-werken" exact component={GyprocWerken}/>
+                        <Route path="/gyproc-werken" exact component={GyprocWerken}/>
 
-                    <Route path="/realisaties" exact component={Realisaties}/>
+                        <Route path="/realisaties" exact component={Realisaties}/>
 
-                    <Route path="/contact" exact component={Contact}/>
+                        <Route path="/contact" exact component={Contact}/>
 
-                    <Route path="/privacy-beleid" exact component={Contact}/>
+                        <Route path="/privacy-beleid" exact component={Contact}/>
 
-                    <Route path="/coockie-beleid" exact component={Contact}/>
+                        <Route path="/coockie-beleid" exact component={Contact}/>
 
-                    <Route path="/algemene-voorwaarden" exact component={Contact}/>
+                        <Route path="/algemene-voorwaarden" exact component={Contact}/>
 
-                </Switch>
-            </BrowserRouter>
+                    </Switch>
+                </ScrollToTop>
+            </Router>
 
         </div>
 
